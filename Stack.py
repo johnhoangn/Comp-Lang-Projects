6da2acac73a57d3777276987f8262734f3506d93
John <johnhoangn@gmail.com>
2019-03-04 12:47:31 -0500

    Comments

--- Stack.py
+++ Stack.py
@@ -1,16 +1,21 @@
+# singly linked node
 class LLNode:
+    # constructor
     def __init__(self, data):
         self.data = data
         self.next = None
 
 
+# linked list implementation of a stack with sentinel nodes
 class Stack:
+    # push to top
     def push(self, value):
         self.num_elements += 1
         new_node = LLNode(value)
         new_node.next = self.head.next
         self.head.next = new_node
 
+    # pop from top, size must be > 0
     def pop(self):
         assert self.size() > 0, "Underflow exception"
         self.num_elements -= 1
@@ -18,17 +23,16 @@ class Stack:
         self.head.next = self.head.next.next
         return ret
 
+    # look at top, size must be > 0
     def peek(self):
         assert self.size() > 0, "Underflow exception"
         return self.head.next.data
 
-    def double_peek(self):
-        assert self.size() > 1, "Underflow exception"
-        return self.head.next.next.data
-
+    # get num elements
     def size(self):
         return self.num_elements
 
+    # debug
     def print(self):
         cur_node = self.head.next
         print("List size (%d), Contents:" % self.size())
@@ -36,6 +40,7 @@ class Stack:
             print("> %s" % cur_node.data)
             cur_node = cur_node.next
 
+    # constructor
     def __init__(self):
         self.num_elements = 0
         self.tail = LLNode(None)
